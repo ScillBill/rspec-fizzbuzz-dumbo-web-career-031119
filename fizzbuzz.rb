a46389e60c0861cb12d22ad37210d29e983c4055
Priscilla Vazquez <scillamae922@gmail.com>
2019-02-26 03:27:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  
+  if num % 5 == 0
+    "Buzz"
+  
+  elseif
+    num % 3 == 0
+    "Fizz"
+
+  elseif
+    num % (3, 5) == 0
+    "FizzBuzz"
+  else
+  num.even?
+  return nil
+  end
+  
+  fizzbuzz(15)
