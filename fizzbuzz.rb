bf9f5b39267dcb32d332016fa338ce3f6440d735
Priscilla Vazquez <scillamae922@gmail.com>
2019-02-26 03:41:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(num)
   if num % 3 == 0 
     puts "Fizz"
-  if int % 5 == 0
+  if num % 5 == 0
     puts "Buzz"
-  if int % 3 && 5 == 0
+  if num % 3 && 5 == 0
     puts"FizzBuzz"
-  if int.even
+  if num.even
     nil
   end
 end
