f0636929c82879fbeab087047b6bd5f1baae1606
Priscilla Vazquez <scillamae922@gmail.com>
2019-02-26 03:31:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 
+    puts "Fizz"
   end
 end
 
